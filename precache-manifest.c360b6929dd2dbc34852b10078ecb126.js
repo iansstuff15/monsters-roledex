a307c0e2465bcc989b80b8c2a346f940552c266c
Dirian Powell <dlpowell35@gmail.com>
2020-09-15 19:35:54 +0800

    Updates

---
+++ precache-manifest.c360b6929dd2dbc34852b10078ecb126.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "55c99acbcacd8b91fe87ff1fe49b7a99",
+    "url": "/monsters-rolexdex/index.html"
+  },
+  {
+    "revision": "343e93794d9a1de57703",
+    "url": "/monsters-rolexdex/static/css/main.f7ae68b3.chunk.css"
+  },
+  {
+    "revision": "18e3087de538d91ee632",
+    "url": "/monsters-rolexdex/static/js/2.2a71acd0.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters-rolexdex/static/js/2.2a71acd0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "343e93794d9a1de57703",
+    "url": "/monsters-rolexdex/static/js/main.2f166349.chunk.js"
+  },
+  {
+    "revision": "ef0c3b1e47b3aa908ae1",
+    "url": "/monsters-rolexdex/static/js/runtime-main.771574dc.js"
+  }
+]);
